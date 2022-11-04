c712dd6b03b02e73565bc2e28820edae4e29d985
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-11-04 12:25:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if a == 'y':
         randomnumber6 = chr(random.randint(ord('0'), ord('9')))
         randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
         randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
-        if randomnumber == '676848Vr':
+        if randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter == '676848Vr':
             print("\033[32m| attempting... 676848Vr |\n\tsuccess! [\u2713] ")
             
         print("\033[31m| attempting..." + randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter + " |", flush=True)
